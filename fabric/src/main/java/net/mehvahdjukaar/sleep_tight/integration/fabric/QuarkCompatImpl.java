40d12908ef83b597bd4c237c3d31fe3f67f576e6
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-01-16 21:54:50 +0100

    quark compat

--- fabric/src/main/java/net/mehvahdjukaar/sleep_tight/integration/fabric/QuarkCompatImpl.java
+++
@@ -1,9 +0,0 @@
-package net.mehvahdjukaar.sleep_tight.integration.fabric;
-
-import net.minecraft.world.level.block.state.BlockState;
-
-public class QuarkCompatImpl {
-    public static boolean isVerticalPost(BlockState facingState) {
-        return false;
-    }
-}
