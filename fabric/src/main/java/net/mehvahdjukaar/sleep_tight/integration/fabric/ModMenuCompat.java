d63e12000ec7ffc59ab739488553866c617627a0
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-07-23 08:27:11 +0200

    a

--- fabric/src/main/java/net/mehvahdjukaar/sleep_tight/integration/fabric/ModMenuCompat.java
+++ fabric/src/main/java/net/mehvahdjukaar/sleep_tight/integration/fabric/ModMenuCompat.java
@@ -2,12 +2,15 @@ package net.mehvahdjukaar.sleep_tight.integration.fabric;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import com.terraformersmc.modmenu.event.ModMenuEventHandler;
+import net.mehvahdjukaar.moonlight.core.client.config.MoonlightConfigSelectScreen;
+import net.mehvahdjukaar.sleep_tight.SleepTight;
+import net.minecraft.resources.ResourceLocation;
 
 public class ModMenuCompat implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ModConfigSelectScreen::new;
+        return parent -> MoonlightConfigSelectScreen.create(SleepTight.MOD_ID, parent,
+                ResourceLocation.withDefaultNamespace("textures/block/blue_wool.png"));
     }
 }
